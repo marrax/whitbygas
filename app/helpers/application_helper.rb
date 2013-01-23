3d643058be1da5d5d19b6d5df81466073e6d42f6
Andrew <marrax@localhost.localdomain>
2013-01-23 11:23:25 +0000

    added page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # returns the full page title
+  def full_title(page_title)
+    base_title = " whitbypluming.co.uk Whitby Plumbing "
+    if page_title.empty?
+     base_title
+    else
+     "#{base_title} - #{page_title}"
+    end
+  end
+
 end
